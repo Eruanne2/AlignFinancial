c9ef7ed637807e60b09c89fd3b37330aa9da9958
Charis <bluegrassgal222@gmail.com>
2021-04-03 15:51:21 -0600

    Complete backend auth pattern. Test all routes via ajax calls.

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ["The username and password you entered isn't valid. Try again."], status: 401
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout
+      render json: { message: "Log out successful."}
+    else 
+      render json: { message: "There is no user logged in."}
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  fname           :string           not null
+#  lname           :string           not null
+#  email           :string           not null
+#  address         :string
+#  phone           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, :fname, :lname, :email, presence: true
+  validates :username, :session_token, :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  after_initialize :ensure_session_token
+
+  # associations go here
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, pw)
+    user = User.find_by(username: username)
+    (user && user.is_password?(pw)) ? user : nil
+  end
+
+  def password=(pw)
+    @password = password
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64(16)
+  end
+
+end
