1095257efebf066b9a31c846d2a543ff1c576d3b
Charis <bluegrassgal222@gmail.com>
2021-04-06 18:07:46 -0600

    Add footer and continue styling

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,7 +1,22 @@
-import react from 'React';
+import React from 'react';
 
 function Footer(props){
+  return(
+    <footer>
+      <ul>
+        <a href='https://github.com/Eruanne2'>Github</a>
+        <a href='www.linkedin.com/in/charis-ginn-9abb93173'>LinkedIn</a>
+        <p>CV</p>
+      </ul>
+      A FEW THINGS YOU SHOULD KNOW
 
+      Securities products and services are offered through Ally Invest Securities LLC, member FINRA and SIPC .
+      Bank accounts and mortgage products are provided by Ally Bank, a Member FDIC and Equal Housing Lender NMLS ID 181005.
+      - dropdown to display more boring stuff -
+      ©2009 – 2021 Ally Financial Inc. 
+
+    </footer>
+  )
 };
 
 export default Footer;
