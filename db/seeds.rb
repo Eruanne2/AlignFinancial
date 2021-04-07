d73446b7db963a3dac60f1602ab3cdb13133c261
Charis <bluegrassgal222@gmail.com>
2021-04-07 09:44:34 -0600

    Add DemoUser to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+demoUser = User.create([ { username: "DemoUser", fname: "Demo", lname: "User", email: "example@email.com", address: "1234 Example Ln", phone: "123-456-7890" }])
