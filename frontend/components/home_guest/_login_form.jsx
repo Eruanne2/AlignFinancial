94a822a5c06600cbfd00bd14aa6e81abc20b14b1
Charis <bluegrassgal222@gmail.com>
2021-04-09 11:57:10 -0600

    Import fa icons into loginForm

--- frontend/components/home_guest/_login_form.jsx
+++ frontend/components/home_guest/_login_form.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { login } from '../../actions/session_actions';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 
 const mapStateToProps = state => {
   return { errors: state.errors.sessionErrors }
@@ -41,7 +43,7 @@ class LoginForm extends React.Component{
           </div>
         <form onSubmit={this.userLogin.bind(this)}>
           <span className='error'>
-            {this.props.errors.length > 0 ? <i className='fas fa-exclamation-circle'></i> : null }
+            {this.props.errors.length > 0 ? <i><FontAwesomeIcon icon={faExclamationCircle}/></i> : null }
             {this.props.errors}
           </span>
           <section className='login-inputs'>
