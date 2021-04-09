d75b39e56cc7347d8c961cebb79001f2a59dd7af
Charis <bluegrassgal222@gmail.com>
2021-04-09 11:46:53 -0600

    Add button to splash page

--- frontend/components/home_guest/splash.jsx
+++ frontend/components/home_guest/splash.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SplashNavbar from './splash_navbar';
+import { Link } from 'react-router-dom';
 
 class Splash extends React.Component{
   render(){
@@ -8,6 +9,9 @@ class Splash extends React.Component{
         <SplashNavbar />
         <section className='better-banking-panel'>
           <p>better banking starts <span>here.</span></p>
+          <p>It’s not about finding a place for your money, it’s about finding</p>
+          <p>the right place for your money.</p>
+          <Link to='/open-account'>Open Account</Link>
         </section>
       </div>
     )
