ffd081d4e5416241f038eabc142a5d5c5dc48a30
Charis <bluegrassgal222@gmail.com>
2021-04-12 12:08:46 -0600

    Remove null constraint from memo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_172149) do
+ActiveRecord::Schema.define(version: 2021_04_12_180634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_172149) do
     t.integer "from_acct_id", null: false
     t.integer "to_acct_id", null: false
     t.float "amount", null: false
-    t.string "memo", null: false
+    t.string "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
