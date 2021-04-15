9a5a4fae8e1bb3919fe66d4f6d9be17992849442
Charis <bluegrassgal222@gmail.com>
2021-04-15 13:29:20 -0600

    Continue styling ExternalAccountsForm

--- frontend/components/accounts/external_accounts/external_account_form.jsx
+++ frontend/components/accounts/external_accounts/external_account_form.jsx
@@ -34,26 +34,42 @@ class ExternalAccountForm extends React.Component {
 
   render(){
     return(
-      <div>
-        <form>
-          <label>Accout Type
-            <select onChange={this.updateField('acctType')}>
-              <option value='checkings' >Checking Account</option>
-              <option value='savings'>Savings Account</option>
-            </select>
-          </label>
-          <label>Account Number
-            <input type='text' value={this.state.acctNum} onChange={this.updateField('acctNum')}/>
-          </label>
-          <label>Re-enter Account Number (dead rn)
-            <input type='text' id='verify-acct-num' onChange={this.checkNumsMatch.bind(this)}/>
-          </label>
-          {this.numsMatch ? 'yes' : 'no'}
-          <label>Routing Number
-            <input type='text' value={this.state.routingNum} onChange={this.updateField('routingNum')}/>
-          </label>
-          <button onClick={this.handleSubmit.bind(this)}>Next</button>
-        </form>
+      <div className='external-acct-form'>
+        <h1 id='external-acct-header'>Add External Account</h1>
+        <p>You must be an owner on the account to use it for transfers.</p>
+
+        <div>
+          <label htmlFor='select-type'>ACCOUNT TYPE</label>
+          <select id='select-type' onChange={this.updateField('acctType')}>
+            <option value='checkings' >Checking Account</option>
+            <option value='savings'>Savings Account</option>
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor='acct-nickname' >ACCOUNT NICKNAME</label>
+          <input id='acct-nickname' type='text' value={this.state.nickname} onChange={this.updateField('nickname')}/>
+        </div>
+
+        <img src={window.exampleAccount} alt="an illustration of the bottom of a check with the account number on the left and the routing number on the right" width='600'/>
+          
+        <div>
+          <label htmlFor='ruoting-again'>ROUTING NUMBER</label>
+          <input id='ruoting-again' type='text' value={this.state.routingNum} onChange={this.updateField('routingNum')}/>
+          {this.numsMatch ? '' : <p className='error'>'Account numbers must match.'</p>}
+        </div>
+
+        <div>
+          <label htmlFor='acct-num'>ACCOUNT NUMBER</label>
+          <input id='acct-num'type='text' value={this.state.acctNum} onChange={this.updateField('acctNum')}/>
+        </div>
+          
+        <div>
+          <label htmlFor='acct-num-again'>RE-ENTER ACCOUNT NUMBER (dead rn)</label>
+          <input id='acct-num-again' type='text' id='verify-acct-num' onChange={this.checkNumsMatch.bind(this)}/>
+        </div>
+          
+        <button onClick={this.handleSubmit.bind(this)}>Next</button>
       </div>
     )
   }
--- frontend/components/accounts/external_accounts/manage_external_accounts.jsx
+++ frontend/components/accounts/external_accounts/manage_external_accounts.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { fetchAllAccounts } from '../../../actions/account_actions';
 import Navbar from '../../home_user/navbar';
 import { Link } from 'react-router-dom';
-import AccountsIndex from '../../accounts/accounts_index';
 import ExternalAccountForm from './external_account_form';
 import { connect } from 'react-redux';
 
@@ -16,17 +15,26 @@ const mapDispatchToProps = dispatch => {
 class ManageExternalAccounts extends React.Component{
   constructor(props){
     super(props);
-    this.state = { showForm: false }
+    this.state = { showForm: false, dropdown: 0 }
   };
 
   componentDidMount(){
     this.props.fetchAllAccounts();
-    // is this buggy??
+  };
+
+  toggleForm(e) {
+    e.preventDefault();
+    this.setState({ showForm: !this.state.showForm });
+  }
+
+  toggleDropdown(id) {
+    return e => {
+      (this.state.dropdown === id) ? this.setState({ dropdown: 0 }): this.setState({ dropdown: id});
+    }
   };
 
   render(){
     if (!this.props.accounts) return null;
-    debugger
     return(
       <div className='external-accts-container'>
         <Navbar/>
@@ -46,7 +54,7 @@ class ManageExternalAccounts extends React.Component{
 
           <div className='external-accts-header'>
             <h2>My Other Accounts</h2>
-            <button>+Add Accounts</button>
+            <button onClick={this.toggleForm.bind(this)}>+Add Accounts</button>
           </div>
           {this.state.showForm && <ExternalAccountForm/>}
 
@@ -59,13 +67,12 @@ class ManageExternalAccounts extends React.Component{
             {this.props.accounts.map((acct, idx) => {
               if (acct.userId !== window.currentUser.id || !acct.external) return null;
               else return <section key={idx}>
-                <ul className='row'>
+                <ul className='row'  onClick={this.toggleDropdown(acct.id)}>
                   <li>{acct.nickname}••••{acct.acctNum % 10000}</li>
                   <li>{acct.acctType}</li>
                   <li><button>+</button></li>
                 </ul>
-                {/* {this.state.dropdown === acct.id &&  */}
-                { true && 
+                {this.state.dropdown === acct.id && 
                   <ul className='row-dropdown'>
                     <li><h3>More Info</h3></li>
                     <li><h3>Account Number: </h3> <p>{acct.acctNum}</p></li>
