b1ec46b358674ca317afb8c9d2c429e8b6007033
Charis <bluegrassgal222@gmail.com>
2021-04-19 08:00:35 -0600

    testing in heroku

--- frontend/components/transfers/transfers_index.jsx
+++ frontend/components/transfers/transfers_index.jsx
@@ -76,7 +76,7 @@ class TransfersIndex extends React.Component {
           </li>
           {filteredTransfers.map((transfer, idx) => {
             return <li key={idx}>
-              <p>{this.formatDate(transfer.createdAt)}</p>
+              {/* <p>{this.formatDate(transfer.createdAt)}</p> */}
               <p>{accounts[transfer.fromAcctId].nickname}••••{accounts[transfer.fromAcctId].acctNum % 10000}</p>
               <p>{accounts[transfer.toAcctId].nickname}••••{accounts[transfer.toAcctId].acctNum % 10000}</p>
               <p>{formatMoney(transfer.amount)}</p>
