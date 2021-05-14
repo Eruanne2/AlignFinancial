809376fbe5845f8de0988dc0810bea6a154417aa
Charis <bluegrassgal222@gmail.com>
2021-05-14 15:56:07 -0600

    add minimal styling to 404 page

--- frontend/components/page_not_found.jsx
+++ frontend/components/page_not_found.jsx
@@ -5,7 +5,7 @@ import Navbar from './home_user/navbar';
 function PageNotFound(props){
   const link = window.currentUser ? <Link to ='/dashboard'>dashboard</Link> : <Link to ='/'>home page</Link>
   return(
-    <div>
+    <div className='page-not-found'>
       <Navbar/>
       <h1>404</h1>
       <h3>Hmm, we can't seem to find that page. Head back to the {link} and see if you can find it there.</h3>
