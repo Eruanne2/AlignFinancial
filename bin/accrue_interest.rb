4cccb64e35ab447733f5101fc35f1c706f87ce10
Charis <bluegrassgal222@gmail.com>
2021-05-26 09:12:41 -0600

    try a different path to env

--- bin/accrue_interest.rb
+++ bin/accrue_interest.rb
@@ -1,4 +1,4 @@
 ENV['RAILS_ENV'] = "production" # Set to your desired Rails environment name
-require 'config/environment'
+require File.expand_path('../config/environment', File.dirname(__FILE__))
 
 Account.accrue_interest
