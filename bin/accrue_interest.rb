9e854f94a9f360425aef4e6ef23ecc9dfa6c9712
Charis <bluegrassgal222@gmail.com>
2021-06-14 17:14:01 -0600

    set script to only accrue interest on mondays

--- bin/accrue_interest.rb
+++ bin/accrue_interest.rb
@@ -1,4 +1,6 @@
 ENV['RAILS_ENV'] = "production" # Set to your desired Rails environment name
 require File.expand_path('../config/environment', File.dirname(__FILE__))
 
-Account.accrue_interest
+if (new Date().getDay() === 1){
+  Account.accrue_interest
+}
